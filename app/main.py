8ffcc53776e3d17f795ec67cee52babe498e7426
Zulfia <zch.analytics.com>
2024-04-29 16:40:20 +0100

    add 404 response

--- app/main.py
+++ app/main.py
@@ -1,29 +1,37 @@
 import socket # low-level networking interface
 
-def main(): #server logic
+def main(): # server logic
     print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True) #function to create a server socket, listen on localhost, port 4221
+    serv_socket = socket.create_server(("localhost", 4221), reuse_port=True) #function to create a server socket, listen on localhost, port 4221
 
     while True: #loop to keep server running and accepting new connections indefinitely
         try:
-            connection, client_address = server_socket.accept() # function is called again 
+            client_conn, client_add = serv_socket.accept() # function is called again 
             # # returned value is a tuple with two elements: the first is the socket object and the second is the address of the client
-            print(f"Connection from {client_address}")
+            print(f"Connection from {client_add}")
 
             # Read data from the connection
-            request_data = connection.recv(1024).decode()
-            print(f"Received request: {request_data}")
+            client_request_data = client_conn.recv(1024).decode()
+            print(f"Received request: {client_request_data}")
             
-            # The \r\n\r\n at the end of the response is required to indicate the end of the HTTP headers according to the HTTP protocol.
-            response = "HTTP/1.1 200 OK\r\n\r\n" 
+            # Extract path from the request
+            request_lines = client_request_data.split('\r\n')
+            start_line = request_lines[]
+            method, path, _ = start_line.split(' ')
+            print(f"Requested path: {path}")
+
+            # Conditional
+            if path == '/':
+                # The \r\n\r\n at the end of the response is required to indicate the end of the HTTP headers according to the HTTP protocol.
+                http_response = "HTTP/1.1 200 OK\r\n\r\n"
+            else:
+                http_response = "HTTP/1.1 404 Not Found\r\n\r\n" 
             
-            connection.send(response.encode())    
-            # the server_socket.send function is called to send the response to the client. 
             # The response.encode() function is used to convert the string into bytes, as the send function requires data to be in bytes.
+            client_conn.send(http_response.encode())    
            
             # Close the connection socket
-            connection_socket.close()
-
+            client_conn.close()
             
         except Exception as e:
             print(f"An error occurred: {e}")
