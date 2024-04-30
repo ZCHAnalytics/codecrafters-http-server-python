5990ba307249bb335a5d1ff7094e44424ae67922
Zulfia <zch.analytics.com>
2024-04-30 19:09:19 +0100

    test

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ def main():
             # Read data from the connection
             client_request_data = client_connection.recv(1024).decode()
             print(f'Decoded request received from client: {client_request_data}')
+            
+            # Extract path from the request
+            _, path, _ = client_request_data.split(" ", 2)
+            print(f'Extracted path from the request: {path}')
 
             # Split http request into separate lines 
             print(f'Let us splitting http request lines into separate lines!')
@@ -22,9 +26,16 @@ def main():
             print('    Let us extract the User-Agent line from the http request')
             user_agent_line = request_lines[2] if len(request_lines) >= 3 else None
             print('      Hurray! There is a user agent line!')
-        
-            print('...  ... ... Starting the "If" block inside the "Try" block\n') 
-            if user_agent_line:
+            
+            print('...  ... ... Starting "If" block')
+            if path.startswith("/echo/"):
+                random_string = path.split("/")[-1]
+                print(f'Extracted random string from path: {random_string}')
+                http_response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}'
+            elif path == "/":
+                print(f'No specific path provided, so returning the default respose')
+                http_response = "HTTP/1.1 200 OK\r\n\r\n"
+            elif user_agent_line:
                 content_with_version = user_agent_line.split(": ")[1]
                 # Exclude the version number from the User-Agent value
                 content_without_version = content_with_version.split('/')[0]
@@ -33,10 +44,10 @@ def main():
                 content_length = len(content_without_version.encode('utf-8'))   
                 print(f'Value of User-Agent extracted and is: {content_with_version}\n')
                 print(f'Value of User-Agent in length is: {len(content_without_version)}\n')
+                http_response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content_without_version)}\r\n\r\n{content_with_version}'
             else:
                 print("User-Agent header not found in the request\n")
 
-            http_response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(content_without_version)}\r\n\r\n{content_with_version}'
             # Send the response back
             print('Sending the response back and putting the kettle on')
             client_connection.send(http_response.encode('utf-8'))
