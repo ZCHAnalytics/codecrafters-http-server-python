2bc2df2eb36e13120953cfd155695bef59e2b8cd
Zulfia <zch.analytics.com>
2024-04-30 10:22:34 +0100

    test

--- app/main.py
+++ app/main.py
@@ -7,23 +7,23 @@ def main():
     while True:
         try:
             client_conn, client_addr = serv_socket.accept()
-            print(f"Connection from {client_addr}")
+            print(f"The connection from {client_addr}")
 
             # Read data from the connection
             client_request_data = client_conn.recv(1024).decode()
-            print(f"Received request: {client_request_data}")
+            print(f"The received request: {client_request_data}")
             
             # Extract path from the request
             request_lines = client_request_data.split('\r\n')
             start_line = request_lines[0]
             _, path, _ = start_line.split(' ')
-            print(f"Requested path: {path}")
+            print(f"The requested path: {path}")
 
             # Check if the path starts with '/echo/'
             if path.startswith('/echo/'):
                 # Extract the random string from the path
                 _, _, random_string = path.split('/')
-                print(f'Random string: {random_string}')
+                print(f'The random string: {random_string}')
 
                 # Create a response body
                 response_body = random_string
@@ -38,11 +38,10 @@ def main():
                 )
             else:
                 # Respond with 404 Not Found for other paths
-                response = "HTTP/1.1 404 Not Found\r\n\r\n"
+                response = "Error - HTTP/1.1 404 Not Found\r\n\r\n"
    
             # Send the response
             client_conn.send(response.encode())    
-           
             # Close the connection socket
             client_conn.close()
 
