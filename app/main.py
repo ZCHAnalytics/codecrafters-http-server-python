c0d8def6a069290ebcc17fef8492d05c20ff2676
Zulfia <zch.analytics.com>
2024-04-30 16:39:13 +0100

    test

--- app/main.py
+++ app/main.py
@@ -3,28 +3,31 @@ import socket
 def main():
     my_serv_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
+    print('...  Starting the "while True" block...')
     while True:
         client_connection, client_address = my_serv_socket.accept()
-        print(f' My "while True" block starts here and gives the connection from {client_address}')
+        print(f'Accepting the connection from client at this address {client_address}')
 
+        print('...  ... Starting the "try" block inside the "while" block')    
         try:
             # Read data from the connection
             client_request_data = client_connection.recv(1024).decode()
-            print(f' My "try" block that decodes the data from the received request: {client_request_data}')
+            print(f'Decoded request received from client: {client_request_data}')
 
             # Extract path from the request
             _, path, _ = client_request_data.split(" ", 2)
-            print(f' tada! This snippet extracts the path from the multi-line request data which is: {path}')
+            print(f'Extracted path from the request: {path}')
 
+            print('...  ... ... Starting "If" block')
             if path.startswith("/echo/"):
                 random_string = path.split("/")[-1]
-                print(f'If the path starts with "echo" than the random string in the extracted path is: {random_string}')
+                print(f'Extracted random string from path: {random_string}')
                 http_response = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(random_string)}\r\n\r\n{random_string}'
             elif path == "/":
-                print(f'If the path does not "echo"')
+                print(f'No specific path provided, so returning the default respose')
                 http_response = "HTTP/1.1 200 OK\r\n\r\n"
             else:
-                print(f'Submitting the resignation form')
+                print(f'Requsted path not found, returning 404 error')
                 http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
 
             # Send the response back
